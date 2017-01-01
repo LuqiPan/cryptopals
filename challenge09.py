9ef9778da3e25d4811ee9a5900ecf6dcfd8e0227
Luqi Pan <luqi@yelp.com>
2017-01-01 11:40:14 -0800

    Realized that I understood PKCS#7 wrongly

--- challenge09.py
+++ challenge09.py
@@ -1,5 +1,9 @@
-def padding(text, length, char='\x04'):
-    return text + (char * (length - len(text)))
+def padding(text, length):
+    len_diff = length - len(text)
+    char = chr(len_diff)
+    return text + (char * len_diff)
 
 if __name__ == '__main__':
-    print padding('YELLOW SUBMARINE', 20)
+    assert(padding('YELLOW SUBMARINE', 20) == 'YELLOW SUBMARINE' + chr(4) * 4)
+    assert(padding('YELLOW SUBMARINE', 32) == 'YELLOW SUBMARINE' + chr(16) * 16)
+    print 'PASS'
