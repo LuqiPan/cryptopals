6500c2e0b2b2baba90ce58f189e850950cd4e484
Luqi Pan <luqi@yelp.com>
2016-12-31 20:38:43 -0800

    Finished challenge8

---
+++ set1/challenge8.py
@@ -0,0 +1,18 @@
+import pprint
+
+def is_encrypted_with_ECB(line):
+    block_size = 16
+    blocks = [line[i:i+block_size] for i in range(0, len(line), block_size)]
+    if len(blocks) > len(set(blocks)):
+        return True
+
+if __name__ == '__main__':
+    block_size = 16
+
+    for line_number, line in enumerate(open('8.txt', 'r').readlines()):
+        line = line.strip()
+        if is_encrypted_with_ECB(line):
+            print line_number
+            print line
+            #pprint.pprint([line[i:i+block_size] for i in range(0, len(line), block_size)])
+            #pprint.pprint(set([line[i:i+block_size] for i in range(0, len(line), block_size)]))
